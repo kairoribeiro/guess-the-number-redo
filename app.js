86a076beaac5ee7cd4c09d2e2edffd80914e73c9
Kairo Ribeiro <kairo10@gmail.com>
2022-09-20 11:58:23 -0400

    add error and render functions

--- app.js
+++ app.js
@@ -20,12 +20,14 @@ const prevGuessMsg = document.querySelector("#prev-guesses-msg")
 
 /*----------------------------- Event Listeners -----------------------------*/
 form.addEventListener("reset", init)
+form.addEventListener("submit", submit)
 
 
 /*-------------------------------- Functions --------------------------------*/
-// init()
+init()
 
 function init() { // when you init the game - reset
+  messageEl.className = ""
   guessesEl.textContent = "" //empty the prev guesses div
   messageEl.textContent = "Please enter a guess between 1 and 100!"
   resetBtn.setAttribute("hidden", true)
@@ -33,4 +35,57 @@ function init() { // when you init the game - reset
   isWinner = false // if theres not a winner
   guessList = [] //empty the guess list
   secretNum = Math.floor(Math.random() * 100 + 1) //random secret num
+  console.log(secretNum)
+}
+
+function submit(evt) {
+  evt.preventDefault() // prevent page from restart when you submit a form
+  if (isWinner === false) {
+    checkGuess(parseInt(guessInput.value))
+  }
+}
+
+function checkGuess(guess) {
+  guessInput.value = ""
+  if (isNaN(guess) || guess < 1 || guess > 100) {
+    renderError("Whoops! Please enter a number from 1 to 100.")
+  }
+  else if (guess === secretNum) {
+    isWinner = true
+    messageEl.textContent = "We have a winner!"
+  } else {
+    guessList.push(guess)
+    render()
+  }
+  console.log(guessList)
+}
+
+function renderError(error) {
+  messageEl.textContent = error
+  messageEl.className = "error"
+}
+
+function render() {
+  const lastGuess = guessList[guessList.length - 1]
+  const div = document.createElement('div')
+  div.textContent = lastGuess
+
+  if (guessList.length === 1) {
+    prevGuessMsg.textContent = "Previous Guesses"
+    resetBtn.removeAttribute("hidden")
+  }
+
+  if (isWinner) {
+    renderWin()
+  } else if (lastGuess > secretNum || lastGuess < secretNum) {
+    renderGuess(div, lastGuess)
+  }
+}
+
+function renderWin () {
+
+}
+
+function renderGuess() {
+
 }
