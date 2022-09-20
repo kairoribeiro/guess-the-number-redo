cdd0a76d82b70e4bc063e138cc3fa23f176ab359
Kairo Ribeiro <kairo10@gmail.com>
2022-09-20 10:06:27 -0400

    add cached elements

--- app.js
+++ app.js
@@ -5,12 +5,18 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+let secretNum
+let guessList
+let isWinner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const form = document.querySelector("form")
+const guessInput = document.querySelector("#guess-input")
+const guessesEl = document.querySelector("#prev-guesses")
+const messageEl = document.querySelector("#message")
+const resetBtn = document.querySelector("#reset-button")
+const prevGuessMsg = document.querySelector("#prev-guesses-msg")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
